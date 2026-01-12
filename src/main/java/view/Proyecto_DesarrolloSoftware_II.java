a1dbd9e3bc26b735a02f4572f0685d250a0bc7d1
50687 <riveraeilyn18@gmail.com>
2026-01-11 18:55:24 -0600

    Paquetes MVC

--- src/main/java/view/Proyecto_DesarrolloSoftware_II.java
+++ src/main/java/view/Proyecto_DesarrolloSoftware_II.java
@@ -11,6 +11,6 @@ package view;
 public class Proyecto_DesarrolloSoftware_II {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hola mundo");
     }
 }
