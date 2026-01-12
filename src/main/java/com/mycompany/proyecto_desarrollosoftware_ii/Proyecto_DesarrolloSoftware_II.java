3d13cd61d48d6c3e81f7478e87a7cc8ab485e036
Kendall <KENDALL.MENDEZCALDERON@ucr.ac.cr>
2026-01-11 18:19:29 -0600

    Initial Commit

---
+++ src/main/java/com/mycompany/proyecto_desarrollosoftware_ii/Proyecto_DesarrolloSoftware_II.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_desarrollosoftware_ii;
+
+/**
+ *
+ * @author user
+ */
+public class Proyecto_DesarrolloSoftware_II {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
